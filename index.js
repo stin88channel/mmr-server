e96b7b421d27c4f7bfd4cc78e66ba28d1ff53327
stin88channel <mirasif77aliev@bk.ru>
2024-12-22 00:13:08 +0300

    fix suka

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: "https://mmr-client.vercel.app/",
+    origin: "http://mmr-client.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
