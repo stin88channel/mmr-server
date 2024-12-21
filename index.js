45418dc2f67faf9c5dad323918a6dce44b41412a
stin88channel <mirasif77aliev@bk.ru>
2024-12-21 22:00:18 +0300

    change api

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://mmr-client.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
