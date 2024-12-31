d4982c8125fec0c9a082ba6e0a6579b4285d7b7f
stin88channel <mirasif77aliev@bk.ru>
2024-12-31 19:16:04 +0300

    deleted require

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const cors = require("cors");
-const User = require("../models/User");
+const User = ("../models/User");
 const UsdtService = require("../UsdtService");
 const {
   test,
